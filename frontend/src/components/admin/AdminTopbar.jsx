914b15292b5c31807bf8823284475137d28c9b54
Vikhnesh-Sathyan <Vikhneshsathyan007@gmail.com>
2026-08-07 18:54:46 +0530

    Build admin sidebar module

--- frontend/src/components/admin/AdminTopbar.jsx
+++ frontend/src/components/admin/AdminTopbar.jsx
@@ -1,22 +1,80 @@
+import { useState } from "react";
+import { ChevronDown, ChevronUp, Bell, Search, User } from "lucide-react";
+import "../../styles/admintopbar.css";
+
+
 function AdminTopbar() {
+
+    const [open, setOpen] = useState(false);
+
 
     return (
 
-        <div
-            style={{
-                height: "80px",
-                background: "#111827",
-                color: "white",
-                display: "flex",
-                alignItems: "center",
-                padding: "0 30px"
-            }}
-        >
-            Topbar
+        <div className={`admin-top-wrapper ${open ? "open" : ""}`}>
+
+
+            {/* Topbar */}
+            <div className="admin-topbar">
+
+                <div className="top-left">
+                    <h2>
+                        Admin Dashboard
+                    </h2>
+                </div>
+
+
+                <div className="top-actions">
+
+                    <div className="icon-box">
+                        <Search size={20}/>
+                    </div>
+
+                    <div className="icon-box">
+                        <Bell size={20}/>
+                    </div>
+
+
+                    <div className="profile">
+
+                        <User size={20}/>
+
+                        <span>
+                            Admin
+                        </span>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+
+
+            {/* Floating Center Button */}
+
+            <button
+                className="toggle-btn"
+                onClick={()=>setOpen(!open)}
+            >
+
+                {
+                    open 
+                    ?
+                    <ChevronUp size={25}/>
+                    :
+                    <ChevronDown size={25}/>
+                }
+
+
+            </button>
+
+
+
         </div>
 
     );
-
 }
+
 
 export default AdminTopbar;

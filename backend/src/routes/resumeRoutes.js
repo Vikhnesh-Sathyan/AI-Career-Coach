2423f382b127c7ec429e59dce2ab7c7c21cd212a
Vikhnesh-Sathyan <Vikhneshsathyan007@gmail.com>
2026-09-02 11:28:20 +0530

    modify resume routes

--- backend/src/routes/resumeRoutes.js
+++ backend/src/routes/resumeRoutes.js
@@ -1,15 +1,50 @@
 import express from "express";
+
 import upload from "../middleware/uploadMiddleware.js";
-import { uploadResume } from "../controllers/resumeController.js";
-import { protect } from "../middleware/authMiddleware.js";
+
+import {
+    uploadResume,
+    getLatestResumeAnalysis
+} from "../controllers/resumeController.js";
+
+import {
+    protect
+} from "../middleware/authMiddleware.js";
+
 
 const router = express.Router();
 
+
+// ==========================================
+// UPLOAD + ANALYZE RESUME
+// ==========================================
+
 router.post(
+
     "/upload",
+
     protect,
+
     upload.single("resume"),
+
     uploadResume
+
 );
+
+
+// ==========================================
+// GET LATEST RESUME ANALYSIS
+// ==========================================
+
+router.get(
+
+    "/latest",
+
+    protect,
+
+    getLatestResumeAnalysis
+
+);
+
 
 export default router;
